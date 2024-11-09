1defd1bfd8ca59b5f2f70c2eb5f4d74fafd7290c
robeb <robbe.stuer@gmail.com>
2024-11-09 13:27:35 +0100

    Possibly more efficient now

--- adfgvx/column_transposition.py
+++ adfgvx/column_transposition.py
@@ -13,22 +13,17 @@ def get_transposition_chi_values(text: str, keys: list[tuple[int, ...]]) -> dict
     :param keys: the list of key orders
     :return: the key orders with their corresponding chi-squared values
     """
-    transpositions = dict()
-    iteration = 0
-    for key in keys:
+    transpositions = {}
+    for iteration, key in enumerate(keys):
         # Get transposition
         transposed = reverse_transpose(text, key)
         # Calculate Frequencies
         frequencies = get_letter_frequencies(transposed)
         # Calculate Chi-Squared value
-        chi_values = []
-        for table in tables:
-            xi_squared = get_xi_squared_value(frequencies, table)
-            chi_values.append(xi_squared)
+        chi_values = [get_xi_squared_value(frequencies, table) for table in tables]
         transpositions[key] = chi_values
         if iteration % 10000 == 0:
             pb(iteration, len(keys))
-        iteration += 1
     return transpositions
 
 
@@ -42,25 +37,27 @@ def reverse_transpose(text, key: tuple[int, ...]) -> str:
     """
     text_len = len(text)
     amount_of_keys = len(key)
-    transposed_list = ["" for _ in range(amount_of_keys)]
-    # Get the amount of letters in each column
-    amount_of_letters = dict()
-    for i in range(amount_of_keys):
-        amount_of_letters[key[i]] = text_len // amount_of_keys
-        # Add one to the amount of letters if there is remainder in text_len / amount_of_keys
-        if i < text_len % amount_of_keys:
-            amount_of_letters[key[i]] += 1
+
+    # Calculate the number of characters in each column
+    base_length = text_len // amount_of_keys
+    remainder = text_len % amount_of_keys
+
+    amount_of_letters = {key[i]: base_length + (1 if i < remainder else 0) for i in range(amount_of_keys)}
+
+    transposed_list = [''] * amount_of_keys
     for i in range(amount_of_keys):
         # Get first amount_of_letters[i] characters from string
         transposed_list[i] = text[0:amount_of_letters[i]]
         # Remove first amount_of_letters[i] characters from string
         text = text[amount_of_letters[i]:]
-    transposed = str()
+
+    # Build transposed string
+    transposed = []
     for i in range(text_len // amount_of_keys + 1):
         for j in key:
             if i < len(transposed_list[j]):
-                transposed += transposed_list[j][i]
-    return transposed
+                transposed.append(transposed_list[j][i])
+    return ''.join(transposed)
 
 
 def get_letter_frequencies(text) -> dict[str, float]:
@@ -71,14 +68,11 @@ def get_letter_frequencies(text) -> dict[str, float]:
     :param text: The text
     :return: The letter frequencies
     """
-    text_length = len(text)/2
-    text = [text[i:i+2] for i in range(0, len(text) - 1, 2)]
-    # Sort the occurences
-    occurences = list(Counter(text).items())
-    occurences.sort(key=lambda x: x[1], reverse=True)
+    bigram_counts = Counter(text[i:i + 2] for i in range(0, len(text) - 1, 2))
+    total = sum(bigram_counts.values())
 
-    # Return it in dict format
-    return {letter: round(letter_count / text_length * 100, 2) for letter, letter_count in occurences}
+    sorted_bigrams = sorted(bigram_counts.items(), key=lambda x: x[1], reverse=True)
+    return {k: round(100 * v / total, 2) for k, v in sorted_bigrams}
 
 
 def get_xi_squared_value(frequencies, expected_frequencies) -> float:
@@ -89,10 +83,8 @@ def get_xi_squared_value(frequencies, expected_frequencies) -> float:
     :param expected_frequencies: The expected frequencies
     :return: The xi squared value
     """
-    # Calculate for 10 highest frequencies
-    xi_squared = 0
-    for i in range(10):
-        observed = frequencies[list(frequencies.keys())[i]]
-        expected = expected_frequencies[list(expected_frequencies.keys())[i]]
-        xi_squared += ((observed - expected) ** 2) / expected
+    # Assume no numbers in text
+    if len(frequencies) > 26:
+        return float('inf')
+    xi_squared = sum(((observed - expected) ** 2) / expected for observed, expected in zip(frequencies.values(), expected_frequencies.values()))
     return xi_squared
